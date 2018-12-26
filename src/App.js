30c83910f2479ab8497e072cb51aa8aab6376ccb
sushantahujaa <Sushantahuja35@gmail.com>
2018-12-26 23:13:12 +0530

    Adds the start Quiz button Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './buttons/StartQuiz';
+import StartQuizButton from "./buttons/StartQuiz";
 
 class App extends Component {
   render() {
@@ -19,6 +21,7 @@ class App extends Component {
           >
             Learn React
           </a>
+            <StartQuizButton/>
         </header>
       </div>
     );
---
+++ src/buttons/StartQuiz.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StartQuizButton extends Component {
+    render() {
+        return (
+            <button>Start Quiz --> </button>
+        );
+    }
+}
+
+export default StartQuizButton;
