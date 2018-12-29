4bcc5037aa058763a3921909f1ab7f87e46c9f94
mounika.puppala <mounika.puppala@servicenow.com>
2018-12-29 14:17:46 +0530

    Removes unused variable - Link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from './dashboard/Dashboard';
 import Quiz from './quiz/Quiz';
 
