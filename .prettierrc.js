416802ff589445d55d35b48c8972c3f845e57907
Manu Dwivedi <manu@curvetomorrow.com.au>
2018-12-29 14:47:34 +0530

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  parser: 'babylon',
+};
